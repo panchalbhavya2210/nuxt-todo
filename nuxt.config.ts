41edfa26bce0d3de1dae604d1db827ef0a0e0375
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-11-26 18:05:18 +0530

    first commit supabase add

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    redirect: false
+    },
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+    css: ['~/assets/css/main.css'],
+})
